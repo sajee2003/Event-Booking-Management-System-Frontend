3d043b30bc4be1bfc32fd71a9332346fbe333763
sajee2003 <kanesarajasajeevan295@gmail.com>
2025-04-27 11:26:25 +0530

    first commit

---
+++ src/app/pages/sample/sample.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  standalone: false,
+  templateUrl: './sample.component.html',
+  styleUrl: './sample.component.css'
+})
+export class SampleComponent {
+
+}
